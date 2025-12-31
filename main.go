1e989d15c4ee597bbd3dfc059e2d7e4dfcc34d35
Jaidev Singh <jaddybanna@gmail.com>
2025-12-31 14:35:32 +0000

    ch4 l6

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
+	"slices"
+	"strings"
 	"sync/atomic"
 )
 
@@ -21,10 +24,17 @@ func handlerHealthz(w http.ResponseWriter, _ *http.Request) {
 
 func (cgf *apiConfig) handlerMatrix(w http.ResponseWriter, _ *http.Request) {
 	header := w.Header()
-	header["Content-Type"] = []string{"text/plain; charset=utf-8"}
+	header["Content-Type"] = []string{"text/html; charset=utf-8"}
 	w.WriteHeader(200)
 	hits := cgf.fileserverHits.Load()
-	res := fmt.Sprintf("Hits: %d", hits)
+	res := fmt.Sprintf(`
+	<html>
+		<body>
+			<h1>Welcome, Chirpy Admin</h1>
+			<p>Chirpy has been visited %d times!</p>
+		</body>
+	</html>
+`, hits)
 	code, err := w.Write([]byte(res))
 
 	if err != nil {
@@ -44,6 +54,56 @@ func (cgf *apiConfig) handlerReset(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
+func handlerChirpValidation(w http.ResponseWriter, r *http.Request) {
+
+	type resError struct {
+		Error string `json:"error"`
+	}
+
+	incomingData := struct {
+		Body string `json:"body"`
+	}{}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+
+	if err := decoder.Decode(&incomingData); err != nil {
+		errorBody, _ := json.Marshal(resError{Error: "Something went wrong!"})
+		w.WriteHeader(500)
+		w.Write(errorBody)
+	}
+
+	if len(incomingData.Body) > 140 {
+		errorBody, _ := json.Marshal(resError{Error: "Chirp is too long"})
+		w.WriteHeader(400)
+		w.Write(errorBody)
+	}
+
+	w.WriteHeader(200)
+
+	chirpSlice := strings.Split(incomingData.Body, " ")
+	wordsToReplace := []string{"kerfuffle", "sharbert", "fornax"}
+	for i, word := range chirpSlice {
+		if slices.Contains(wordsToReplace, strings.ToLower(word)) {
+			chirpSlice[i] = "****"
+		}
+	}
+
+	body, err := json.Marshal(struct {
+		CleanedBody string `json:"cleaned_body"`
+	}{CleanedBody: strings.Join(chirpSlice, " ")})
+
+	if err != nil {
+		errorBody, _ := json.Marshal(resError{Error: "Something went wrong!"})
+		w.WriteHeader(500)
+		w.Write(errorBody)
+	}
+
+	w.Write([]byte(body))
+}
+
 func main() {
 
 	config := apiConfig{
@@ -52,11 +112,13 @@ func main() {
 
 	mux := http.ServeMux{}
 
-	mux.HandleFunc("GET /healthz", handlerHealthz)
+	mux.HandleFunc("GET /api/healthz", handlerHealthz)
 
-	mux.HandleFunc("GET /metrics", config.handlerMatrix)
+	mux.HandleFunc("GET /admin/metrics", config.handlerMatrix)
 
-	mux.HandleFunc("POST /reset", config.handlerReset)
+	mux.HandleFunc("POST /admin/reset", config.handlerReset)
+
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpValidation)
 
 	fileServer := http.FileServer(http.Dir("."))
 	mux.Handle("/app/", http.StripPrefix("/app", config.middlewareMetricsInc(fileServer)))
